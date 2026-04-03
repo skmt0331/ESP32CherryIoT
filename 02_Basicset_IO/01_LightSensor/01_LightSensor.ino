a0fe3b1dacbcc99a2eaa345ef3f70e928501cb35
skmt0331 <c.sakamoto1995@gmail.com>
2026-04-03 12:11:54 +0900

    Change threshold

--- 02_Basicset_IO/01_LightSensor/01_LightSensor.ino
+++ 02_Basicset_IO/01_LightSensor/01_LightSensor.ino
@@ -15,11 +15,11 @@ void loop() {
   Serial.print(" Lux : ");
   
   // Change the threshold to suit your environment
-  if (lux > 20) { 
+  if (lux > 70) { 
     Serial.println("Bright");
-    delay(500);
+    delay(300);
   } else {
     Serial.println("Dark");
-    delay(500);
+    delay(300);
   }
 }
