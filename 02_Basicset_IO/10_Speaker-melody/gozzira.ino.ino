b2747b66bc0e4e32b7443bc8d415528662e0b7a8
skmt0331 <c.sakamoto1995@gmail.com>
2026-04-09 13:28:30 +0900

    gozzira song

---
+++ 02_Basicset_IO/10_Speaker-melody/gozzira.ino.ino
@@ -0,0 +1,101 @@
+const int spkrPin = 3; //3:ConnectorA 4:ConnectorB
+
+#define DO 261.6
+#define _DO 277.18
+#define RE 293.665
+#define _RE 311.127
+#define MI 329.63
+#define FA 349.228
+#define _FA 369.994
+#define SO 391.995
+#define _SO 415.305
+#define RA 440
+#define _RA 466.164
+#define TI 493.883
+#define octDO 523.251
+
+void playmusic(){
+  ledcWriteTone(spkrPin, octDO);
+  delay(250);
+  ledcWriteTone(spkrPin, TI);
+  delay(250);
+  ledcWriteTone(spkrPin, RA);
+  delay(400);
+  ledcWriteTone(spkrPin, 0); // no sound
+  delay(50);
+  ledcWriteTone(spkrPin, octDO);
+  delay(250);
+  ledcWriteTone(spkrPin, TI);
+  delay(250);
+  ledcWriteTone(spkrPin, RA);
+  delay(400);
+  ledcWriteTone(spkrPin, 0); // no sound
+  delay(50);
+  ledcWriteTone(spkrPin, octDO);
+  delay(250);
+  ledcWriteTone(spkrPin, TI);
+  delay(250);
+  ledcWriteTone(spkrPin, RA);
+  delay(250);
+  ledcWriteTone(spkrPin, SO);
+  delay(250);
+  ledcWriteTone(spkrPin, RA);
+  delay(250);
+  ledcWriteTone(spkrPin, TI);
+  delay(250);
+  ledcWriteTone(spkrPin, octDO);
+  delay(250);
+  ledcWriteTone(spkrPin, TI);
+  delay(250);
+  ledcWriteTone(spkrPin, RA);
+  delay(400);
+  ledcWriteTone(spkrPin, 0); // no sound
+  delay(50);
+  ledcWriteTone(spkrPin, octDO);
+  delay(250);
+  ledcWriteTone(spkrPin, TI);
+  delay(250);
+  ledcWriteTone(spkrPin, RA);
+  delay(400);
+  ledcWriteTone(spkrPin, 0); // no sound
+  delay(50);
+  ledcWriteTone(spkrPin, octDO);
+  delay(250);
+  ledcWriteTone(spkrPin, TI);
+  delay(250);
+  ledcWriteTone(spkrPin, RA);
+  delay(400);
+  ledcWriteTone(spkrPin, 0); // no sound
+  delay(50);
+  ledcWriteTone(spkrPin, octDO);
+  delay(250);
+  ledcWriteTone(spkrPin, TI);
+  delay(250);
+  ledcWriteTone(spkrPin, RA);
+  delay(250);
+  ledcWriteTone(spkrPin, SO);
+  delay(250);
+  ledcWriteTone(spkrPin, RA);
+  delay(250);
+  ledcWriteTone(spkrPin, TI);
+  delay(250);
+  ledcWriteTone(spkrPin, octDO);
+  delay(250);
+  ledcWriteTone(spkrPin, TI);
+  delay(250);
+  ledcWriteTone(spkrPin, RA);
+  delay(400);
+  ledcWriteTone(spkrPin, 0); // no sound
+  delay(50);
+  
+}
+
+void setup() {
+  pinMode(spkrPin, OUTPUT);
+  ledcAttach(spkrPin, 12000, 8); //Pin setting(Pin num, Max frequency, Resolution)
+}
+
+void loop() {
+  playmusic();
+  delay(3000);
+}
